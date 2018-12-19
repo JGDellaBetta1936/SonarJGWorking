4d31336c9900c6999c4d97455b4c4df26f13180c
CrunchifyRESTJerseyExample02152017 <john_gold@comcast.net>
2018-12-19 16:58:58 -0500

    getting sonar to work again.
    getting sonar to work again.

---
+++ StarterOne.java
@@ -0,0 +1,9 @@
+package com.jg.sonar.v1;
+
+public class StarterOne {
+	static Integer value1 = Integer.valueOf(1);
+	
+	private StarterOne () {
+		throw new IllegalStateException("need to initialize class properly.");
+	}
+}
